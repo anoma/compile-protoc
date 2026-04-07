61a1ef516eec15d998cd119b23fea63b907cba16
mariari <mariari@protonmail.com>
2026-04-07 23:38:17 +0800

    Set compile_protoc package information

--- mix.exs
+++ mix.exs
@@ -12,11 +12,20 @@ defmodule CompileProtoc.MixProject do
       version: version(),
       build_path: "_build",
       config_path: "config/config.exs",
+      package: package(),
       deps_path: "deps",
       lockfile: "mix.lock",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps()
+    ]
+  end
+
+  def package do
+    [
+      maintainers: ["Mariari", " Raymond E. Pasco"],
+      name: :compile_protoc,
+      licenses: ["MIT"]
     ]
   end
 
